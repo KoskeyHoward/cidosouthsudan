f4ec170e31854a25251dd4937bbadf2d26c46989
Tamminga Budds <79473097+budds300@users.noreply.github.com>
2023-09-28 18:41:47 +0300

    init

---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,40 @@
+<div class="min-vh-100" style="background-color: #54595F">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-3">
+            <h3 class="text-uppercase" style="color:#f06f17">About Cido</h3>
+            <p>CIDO was formed by a group of female youth and women who were living in a protection of civilian site in South Sudan after the outbreak of the 2013 South Sudan political crisis that lead to deaths of over 1 million people and displacement of over 5 million people. These women sat together and pondered over the scenarios around them and the disadvantages of instability and they desired change.</p>
+    </div>
+        <div class="col-lg-3">
+            <h3 class="text-uppercase" style="color:#f06f17">Recent Projects</h3>
+            <div class="d-flex justify-content-center">
+                <img src="https://web.archive.org/web/20221209094222im_/https://cidosouthsudan.org/wp-content/uploads/2022/10/cido-ahec_staff-100x75.jpg" alt="">
+                <p>CIDO Partners with IntraHealth
+                </p>
+            </div>
+            <div class="d-flex justify-content-center">
+                <a class="post-title-color nav-link" href="https://web.archive.org/web/20221209094222/https://cidosouthsudan.org/power-of-integrated-services-in-akobo-east/">Power of Integrated Services i</a>
+                <a class="post-title-color nav-link" href="https://web.archive.org/web/20221209094222/https://cidosouthsudan.org/cido-saves-a-woman-3-days-of-labor-pain-in-akobo/">CIDO SAVES A WOMAN 3 DAYS OF L</a>
+            </div>
+    </div>
+        <div class="col-lg-3">
+            <h3 class="text-uppercase" style="color:#f06f17">Recent causes</h3>
+            <p>CIDO was formed by a group of female youth and women who were living in a protection of civilian site in South Sudan after the outbreak of the 2013 South Sudan political crisis that lead to deaths of over 1 million people and displacement of over 5 million people. These women sat together and pondered over the scenarios around them and the disadvantages of instability and they desired change.</p>
+    </div>
+        <div class="col-lg-3">
+            <h3 class="text-uppercase" style="color:#f06f17">Contact Information</h3>
+            <p>Email: info@cidosouthsudan.org</p>
+            <p>Phone: +211916756740</p>
+    </div>
+</div>
+</div>
+<footer class="d-flex justify-content-around pt-3 pb-3 bg-black">
+
+    <div>
+      <h5 style="color: #f58220;"> &copy; CIDO South Sudan | All rights reserved</h5>
+    </div>
+    <div>
+      <a href="" class="fa fa-facebook nav-link text-white" style="text-decoration: none;"></a>
+      <a href="" class="fa fa-twitter ms-4 nav-link text-white" style="text-decoration: none;"></a>
+    </div>
+  </footer>
