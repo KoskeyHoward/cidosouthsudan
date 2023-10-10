040aa84d21b12a3d8a59d8c1a083fdbaefcba1b5
james <jamesmaloba94@gmail.com>
2023-10-10 16:14:54 +0300

    home page cosistent colors and button, programs page visible buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,7 +75,7 @@
       </div>
 
       <!-- who we are  -->
-      <div class="row" style="background-color: #f58220;">
+      <div class="row" style="background-color: #00A651;">
         <!-- image  -->
         <div class="container col-md-6 pt-2 ps-4 pb-2 mb-md-0 mb-lg-0" style="height: 100%;">
             <img src="{{URL('images/SV-launch2.webp')}}" alt="who we are" style="width: 100%; height:356px; ;object-fit: cover; border-radius: 8px;">
@@ -88,7 +88,7 @@
                     <h5 class="card-title fs-1 mb-lg-3 mb-sm-3  mb-2">Who We Are</h5>
                     <p class="card-text text-secondary mb-lg-4 mb-2 mb-sm-3 fs-5">CIDO is a non-partisan, non-profit making organization conceived in 2013 and formally registered in South Sudan in 2016 to support local solutions on critical areas of social and economic development. The agency places emphasis on promotion of Peace and peaceful co-existence among communities as the benchmark for socio-economic prosperity. </p>
                     <p class="card-text text-swcondary mb-lg-4 mb-2 mb-sm-3 fs-6"></p>
-                    <a href="/about" style="text-decoration: none;" class="card-link text-dark"><button type="button" class="btn align-content-center mb-lg-3 mb-0" style="background-color: #f58220;">Learn More</button></a>
+                    <button type="button" class="learn-btn align-content-center mb-lg-3 mb-0"><a href="/about" style="text-decoration: none;" class="card-link text-light p-2">Learn More</a></button>
                   </div>
                 </div>
               </div>
@@ -122,9 +122,9 @@
     </section>
 
     <!-- programs  -->
-    <section class="text-center p-5">
+    <section class="text-center p-5 programs">
       <div class="cido-programs mb-4 mb-lg-0">
-        <h2> CIDO PROGRAMS</h2>
+        <h2 class="orange"> CIDO PROGRAMS</h2>
         <h5>Activities of CIDO currently covers Central Equitoria state (kajokeji, Lainya Juba and Yei) <br> with planned extension to Eastern Equitoria ( Torit and Ikotos), Upper Nile ( Panyikang, Tonga, Maban) <br> Jonglei state ( Pigi, and Ayod ).</h5>
       </div>
 
@@ -135,7 +135,7 @@
                   <div class="image-container">
                     <div class="overlay d-inline-block" style="padding-top:55px;">
                       <h5 class="text-light mb-3">Peace and Security</h5>
-                        <button class="button"><a class="btn" href="/peace-building">View Program</a></button>
+                      <a class="btn button" href="/peace-building">View Program</a>
                     </div>
                     <img src="{{asset('images/peacebuilding.jpeg')}}" alt="Your Image"id="zoomable-image">
                   </div>
@@ -146,7 +146,7 @@
                   <div class="image-container">
                     <div class="overlay d-inline-block" style="padding-top:55px;">
                       <h5 class="text-light mb-3">Women Rights and Empowerment</h5>
-                        <button class="button"><a href="/women-rights">View Program</a></button>
+                      <a class="btn button" href="/women-rights">View Program</a>
                     </div>
                     <img src="{{asset('images/Livelihood.jpeg')}}" alt="Your Image"id="zoomable-image">
                   </div>
@@ -166,7 +166,7 @@
           </div>
       </div>
 
-      <button class="btn bg-success"> <a class="programs-btn" href="/programs" style="text-decoration: none; color: white;"> More Programs >></a></button>
+      <button class="btn programs-btn"> <a href="/programs" style="text-decoration: none; color: white;"> More Programs >></a></button>
     </section>
 
     <!-- team  -->
@@ -178,7 +178,7 @@
     </section> -->
 
     <!-- projects  -->
-    <section class="text-center p-4 pb-5">
+    <section class="text-center p-4 pb-1 projects">
       <div class="mb-4">
         <h2 class="orange">OUR PROJECTS</h2>
         <h5>We continue to reach out to as many people as possible by <br>extending our services which range from nutrition,access to justice, <br> adult literacy to women empowerement, health services and protection.</h5>
@@ -247,7 +247,7 @@
           <!-- more projects  -->
           <div class="row">
             <div class="col-12">
-              <a class="btn orange bg-success pe-5 ps-5" href="/projects"> More projects >></a>
+            <button class="btn project-btn"><a href="/projects" style="text-decoration: none; color:white;"> More projects >></a></button>
             </div>
           </div>
         </div>
--- resources/views/programs.blade.php
+++ resources/views/programs.blade.php
@@ -10,14 +10,11 @@
           </div>
     </div>
     <div class="container pt-3">
-        <div class="m-5">
-            <div class="row">
+        <div>
+            <div class="row programs mb-4">
                 <div class="col-lg-4 mb-4 mb-lg-0 c-program">
-                    <div class="card" style="width: 19rem;">
-                        <div class="image-container">
-                            <div class="overlay">
-                                <a class="button btn">View Program</a>
-                            </div>
+                    <div class="card" style="width: 100%;">
+                        <div class="image-container" style="width: 100%;">
                             <img src="{{asset('images/peacebuilding.jpeg')}}" alt="Your Image"id="zoomable-image">
                             </div>
                         <div class="card-body">
@@ -25,57 +22,48 @@
                             <p class="card-text">CIDO works with the relevant national and international actors to promote
                                 peaceful co-existence among communities through active engagement of stakeholders in peace
                                 processes.</p>
-
+                            <a class="btn programs-btn" href="/peace-building">View Program</a>
                         </div>
                     </div>
                     </div>
                 <div class="col-lg-4 mb-4 mb-lg-0 c-program">
-                    <div class="card" style="width: 19rem;">
-                        <div class="image-container">
-                            <div class="overlay">
-                                <a class="button btn">View Program</a>
-                            </div>
+                    <div class="card" style="width: 100%;">
+                        <div class="image-container" style="width: 100%;">
                             <img src="{{asset('images/Livelihood.jpeg')}}" alt="Your Image"id="zoomable-image">
                             </div>
                         <div class="card-body">
                             <h5 class="card-title">Women Rights and Empowerement</h5>
                             <p class="card-text">Under the protection Department, CIDO responds to cases of Gender based violence through direct support, case management , Psycho-social support and gender mainstreaming.</p>
-
+                            <a class="btn programs-btn" href="/women-rights">View Program</a>
                         </div>
                     </div>
                    
                 </div>
                 <div class="col-lg-4 mb-4 mb-lg-0 c-program">
-                    <div class="card" style="width: 19rem;">
-                        <div class="image-container">
-                            <div class="overlay">
-                                <a class="button btn">View Program</a>
-                            </div>
+                    <div class="card" style="width: 100%;">
+                        <div class="image-container" style="width: 100%;">
                             <img src="{{asset('images/GL.jpeg')}}" alt="Your Image"id="zoomable-image">
                             </div>
                         <div class="card-body">
                             <h5 class="card-title">Access To Justice</h5>
                             <p class="card-text">CIDO works with a team of lawyers and paralegals to ensure that vulnerable communities access Justice through Legal information and counselling, pro Bono services...</p>
-
+                            <a class="btn programs-btn" href="#">View Program</a>
                         </div>
                     </div>
                    
                 </div>
             </div>
 
-            <div class="row">
+            <div class="row programs mb-4">
                 <div class="col-lg-4 mb-4 mb-lg-0 c-program">
-                <div class="card" style="width: 19rem;">
-                        <div class="image-container">
-                            <div class="overlay">
-                                <a class="button btn">View Program</a>
-                            </div>
+                <div class="card" style="width: 100%;">
+                        <div class="image-container" style="width: 100%;">
                             <img src="{{asset('images/health-delivery.jpeg')}}" alt="Your Image"id="zoomable-image">
                             </div>
                         <div class="card-body">
                             <h5 class="card-title">Health and Nutrition</h5>
                             <p class="card-text">CIDO is working in close collaboration with the ministry of Health to deliver basic health packages and reduce malnutrition in various counties of South...</p>
-
+                            <a class="btn programs-btn" href="/health-and-nutrition">View Program</a>
                         </div>
                     </div>
                 </div>
